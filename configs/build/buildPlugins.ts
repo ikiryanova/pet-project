ceafb1b254dfdc5dbb420d16b7bf9ee74534677e
Irina <irinakir98@gmail.com>
2024-10-12 19:45:27 +0300

    Webpack hot module replacement

--- configs/build/buildPlugins.ts
+++ configs/build/buildPlugins.ts
@@ -2,9 +2,10 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -15,6 +16,15 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
-  ]
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new ReactRefreshWebpackPlugin()
+    );
+  }
+
+  return plugins;
 }
