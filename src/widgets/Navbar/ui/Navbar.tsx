d216631b7dd810523a9ec228d9b8ab1a5eea5c5c
Irina <irinakir98@gmail.com>
2024-10-27 16:18:38 +0300

    added Loader and added NotFoundPage

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,20 @@
+import { useTranslation } from "react-i18next";
 import { AppLink } from "shared/ui";
 import { AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
 import cls from "./Navbar.module.scss";
 
 export const Navbar = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={cls.Navbar}>
       <div className={cls.links}>
         <AppLink to="/" className={cls.mainLink} theme={AppLinkTheme.SECONDATY}>
-          Главная
+          {t("Главная")}
         </AppLink>
         <AppLink to="/about" theme={AppLinkTheme.SECONDATY}>
-          О сайте
+          {t("О сайте")}
         </AppLink>
       </div>
     </div>
