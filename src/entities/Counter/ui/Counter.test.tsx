fab0bc25499454551ffa229e8e06f0fab07e3c48
Irina <irinakir98@gmail.com>
2025-01-25 18:59:18 +0300

    added redux-toolkit, added counter

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,28 @@
+import { screen } from "@testing-library/react";
+import { userEvent } from "@storybook/testing-library";
+
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
+import { Counter } from "./Counter";
+
+describe("Counter", () => {
+  test("Test render", () => {
+    componentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    expect(screen.getByTestId("counter-title")).toHaveTextContent("10");
+  });
+  test("decrement", () => {
+    componentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    userEvent.click(screen.getByTestId("decrement-btn"));
+    expect(screen.getByTestId("counter-title")).toHaveTextContent("9");
+  });
+  test("increment", () => {
+    componentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    userEvent.click(screen.getByTestId("increment-btn"));
+    expect(screen.getByTestId("counter-title")).toHaveTextContent("11");
+  });
+});
