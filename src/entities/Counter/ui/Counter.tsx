ee048fed532e9d2041d1856058c389be4411465d
Irina <irinakir98@gmail.com>
2025-05-15 21:08:16 +0300

    added input and login modal form

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,8 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "shared/ui/Button/Button";
 
+import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
 import { counterActions } from "../model/slice/counterSlice";
-import { getCounterValue } from "../selectors/getCounterValue/getCounterValue";
 
 export const Counter = () => {
   const dispatch = useDispatch();
